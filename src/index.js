dcbaed9c70362ac00a4cdef736200dad97284331
Dicki Prasetya <semenjakpetang176@gmail.com>
2022-10-08 00:33:20 +0700

    feat: install starting depedencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/assets/css/custom.css";
+import "../src/assets/css/fontawesome.css";
+// import "../src/assets/js/fontawesome";
+// import "../src/assets/css/all.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
